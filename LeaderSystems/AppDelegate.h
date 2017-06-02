282eefdd3201cb3409bcb1df4087df114d38037d
刘艳凯 <liuyankai@liuyankaideMacBook-Pro.local>
2017-06-02 16:27:29 +0800

    Initial Commit

---
+++ LeaderSystems/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeaderSystems
+//
+//  Created by 刘艳凯 on 2017/6/2.
+//  Copyright © 2017年 YiTu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeaderSystems/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeaderSystems
+//
+//  Created by 刘艳凯 on 2017/6/2.
+//  Copyright © 2017年 YiTu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
